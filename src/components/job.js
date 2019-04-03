1a526395c8ded466be641b8c0090d02ef99dbde9
NIXON FERNANDO ORTIZ SUAREZ <nf.ortiz@sis.virtual.uniandes.edu.co>
2019-04-02 21:37:18 -0500

    Falta detectar idioma automaticamente

--- src/components/job.js
+++ src/components/job.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {FormattedDate,FormattedRelative,FormattedPlural,FormattedNumber } from 'react-intl';
 export default class Job extends React.Component {
 
   	render() {
@@ -8,9 +8,20 @@ export default class Job extends React.Component {
   				<th scope="row">{this.props.offer.id}</th>
       			<td>{this.props.offer.name}</td>
   				<td>{this.props.offer.company}</td>
-      			<td>{this.props.offer.salary}</td>
+      			<td>{this.props.offer.salary} <FormattedPlural value={this.props.offer.salary} one='million'
+    other='millions'/> </td>
       			<td>{this.props.offer.city}</td>
-      			<td>{this.props.offer.date}</td>
+      			<td>
+				<FormattedDate
+				value={new Date(this.props.offer.date)}
+				year='numeric'
+				month='long'
+				day='numeric'
+				weekday='long'
+				/> (
+				<FormattedRelative value={new Date(this.props.offer.date)}/>)
+			</td>
+			<td><FormattedNumber  value={this.props.offer.views} /></td>
   			</tr>
   		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider, addLocaleData} from 'react-intl';
 
 import JobsList from "./components/jobsList";
 
-ReactDOM.render(<JobsList/>, document.getElementById("root"));
+import esLocaleData from 'react-intl/locale-data/es';
+import enLocaleData from 'react-intl/locale-data/en';
+
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
+
+addLocaleData(esLocaleData);
+addLocaleData(enLocaleData);
+
+console.log(window);
+ReactDOM.render(
+	<IntlProvider  locale="es-Es" messages= {localeEsMessages}>
+		<JobsList/>
+	</IntlProvider>, document.getElementById("root")
+);
